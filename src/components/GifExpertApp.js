82149b2e23cc7925e9a633bb23427d256f7573b7
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-18 02:26:30 -0500

    Adding element to the list
    Using useState and spread operator

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState(
+    [
+      'One Punch Man',
+      'Samurai X',
+      'Saint Seiya',
+      'Dragon Ball Z'
+    ]
+  );
+
+  const handleAdd = () => {
+    setCategories([...categories, 'Pokemon']);
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {
+          categories.map( category => {
+            return <li key={ category }> { category }</li>;
+          })
+        }
+      </ol>
     </>
   );
 }
