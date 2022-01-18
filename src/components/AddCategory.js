9151e2e13b00b24478d7133090b9aed855ba29bc
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-18 02:46:57 -0500

    Create AddCategory Component

---
+++ src/components/AddCategory.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export const AddCategory = () => {
+
+  const [inputValue, setInputValue] = useState('Hola Mundo');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // evita comportamiento por default
+    console.log('Submit hehe')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+
+}
