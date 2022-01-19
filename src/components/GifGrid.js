d8d4217a9ba2bf1d732791a1311d1ca519602f1a
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-19 15:35:07 -0500

    Create GifGridItem Component

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
-  
-  const [count, setCount] = useState(0);
+
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     getGifs();
@@ -20,14 +21,21 @@ export const GifGrid = ({category}) => {
     }))
 
     console.log(gifs);
+    setImages(gifs);
   }
 
 
   return (
     <>
       <h3> {category} </h3>
-      <h3> {count} </h3>
-      <button onClick={ () => setCount(count + 1) }></button>
+        {
+          images.map((images) =>
+            <GifGridItem
+              key={images.id}
+              {...images}
+            />
+          )
+        }
     </>
   );
 }
