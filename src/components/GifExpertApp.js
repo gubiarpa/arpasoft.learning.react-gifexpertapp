35ff9056cd2263a8bc0b9ed250d7c550d25d4ce1
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-19 11:12:47 -0500

    useEffects in components

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
 
 const GifExpertApp = () => {
   
-  const [categories, setCategories] = useState(
-    [
-      'One Punch Man',
-      'Samurai X',
-      'Saint Seiya',
-      'Dragon Ball Z'
-    ]
-  );
+  const [categories, setCategories] = useState(['Saint Seiya']);
 
   return (
     <>
@@ -20,9 +14,12 @@ const GifExpertApp = () => {
 
       <ol>
         {
-          categories.map( category => {
-            return <li key={ category }> { category }</li>;
-          })
+          categories.map( category =>
+            <GifGrid
+              key={category}
+              category={category}
+            />
+          )
         }
       </ol>
     </>
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const GifGrid = ({category}) => {
   
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async() => {
     const url = `https://api.giphy.com/v1/stickers/search?q=Dragon Ball Z&limit=10&api_key=PFVvhfL0JApPVrMjn6jA3a73AnLeedP8`;
     const resp = await fetch(url);
@@ -16,11 +22,12 @@ export const GifGrid = ({category}) => {
     console.log(gifs);
   }
 
-  getGifs();
 
   return (
     <>
       <h3> {category} </h3>
+      <h3> {count} </h3>
+      <button onClick={ () => setCount(count + 1) }></button>
     </>
   );
 }
