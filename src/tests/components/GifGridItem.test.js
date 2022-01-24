585d60671839fe2014598c93d042923016f10bb7
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-23 20:47:45 -0500

    Adding Testing to GifGridItem Component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,12 +5,29 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
 
+  const title = 'Saitama Sensei';
+  const url = 'https://media0.giphy.com/media/Y3rwoQDYUOQBKF51Kw/giphy.gif?cid=9a21f0474a1u0ciz9b9isalrnop97x7oxtebvbpvr40htw7s&rid=giphy.gif&ct=s';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
   test('Debe mostrar <GifGridItem /> correctamente', () => {
-    const title = 'Saitama Sensei';
-    const url = 'https://media0.giphy.com/media/Y3rwoQDYUOQBKF51Kw/giphy.gif?cid=9a21f0474a1u0ciz9b9isalrnop97x7oxtebvbpvr40htw7s&rid=giphy.gif&ct=s';
-    // const wrapper = shallow(<GifGridItem />);
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Debe tener un párrafo con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('Debe tener la imagen igual al url y alt de los props', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('Debe tener la clase animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    expect(className).toContain('animate__fadeIn');
   });
 
 });
