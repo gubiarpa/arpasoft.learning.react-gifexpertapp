9ea6bcd0b5561b4f9eb49fcafddb390aa331fd1a
Billy Arredondo <billy.arredondo@pucp.pe>
2022-01-25 23:32:12 -0500

    Adding Testing to AddCategory Component
    - Simular el `inputChange`
    2. Simular el `submit`
    3. `setCategories` se debe haber llamado
    4. El valor del `input` debe estar vacío

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -20,14 +20,33 @@ describe('Pruebas en <AddCategory />', () => {
   test('Debe cambiar la caja de texto', () => {
     const input = wrapper.find('input');
     const value = 'Hola mundo';
-
+    
     input.simulate('change', { target: { value } });
     expect(wrapper.find('p').text().trim()).toBe(value);
   });
-
+  
   test('No debe postear submit', () => {
     wrapper.find('form').simulate('submit', { preventDefault: () => {} });
     expect(setCategories).not.toHaveBeenCalled();
+  });
+  
+  test('Debe llamar el setCategories y limpiar el textbox', () => {
+    const value = 'Yeri';
+    
+    // 1. Simular el inputChange
+    const input = wrapper.find('input');
+    input.simulate('change', { target: { value } });
+    expect(wrapper.find('p').text().trim()).toBe(value);
+    
+    // 2. Simular el submit
+    const form = wrapper.find('form');
+    form.simulate('submit', { preventDefault() { } });    
+    
+    // 3. setCategories se debe haber llamado
+    expect(setCategories).toHaveBeenCalled();
+    
+    // 4. El valor del input debe estar vacío
+    expect(wrapper.find('input').prop('value')).toBe('');
   });
 
 });
