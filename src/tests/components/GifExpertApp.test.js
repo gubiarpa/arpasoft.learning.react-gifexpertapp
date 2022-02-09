f2bcac3abccb80899f37d7f57bca2ad06a294502
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-09 16:02:59 -0500

    Adding Testing to GifExpertApp Component

---
+++ src/tests/components/GifExpertApp.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import GifExpertApp from '../../components/GifExpertApp';
+
+describe('Pruebas en <GifExpertApp />', () => {
+
+    test('Debe mostrar el componente OK y match con snapshot', () => {
+        const wrapper = shallow(<GifExpertApp />);
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    test('Debe mostrar una lista de categorías', () => {
+        const categories = ['One Punch', 'Dragon Ball'];
+        const wrapper = shallow(<GifExpertApp defaultCategories={ categories } />);
+        
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length);
+    });
+
+});
+
