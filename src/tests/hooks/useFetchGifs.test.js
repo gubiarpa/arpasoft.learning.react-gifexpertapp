671c206c2046d31843a7931bf55ba1081cbaf2f3
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-09 22:32:43 -0500

    Adding waitForNextUpdate Method

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,12 +3,22 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en el hook useFetchGifs', () => {
 
-    test('Debe retornar el estado inicial', () => {
-        const { result } = renderHook(() =>  useFetchGifs('One Punch'));
+    test('Debe retornar el estado inicial', async() => {
+        const { result, waitForNextUpdate } = renderHook(() =>  useFetchGifs('One Punch'));
         const { data, loading } = result.current;
+        await waitForNextUpdate();
         
         expect(data).toEqual([]);
         expect(loading).toEqual(true);
+    });
+    
+    test('Debe retornar un arreglo de imágenes y loading en false', async() => {
+        const { result, waitForNextUpdate } = renderHook(() =>  useFetchGifs('One Punch'));
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+        
+        expect(data.length).toEqual(10);
+        expect(loading).toEqual(false);
     });
 
  }); 
